54f8fe780e15ec9d345724105d578878ff20f95f
gbargiel <50881847+gbargiel@users.noreply.github.com>
2021-09-23 18:35:31 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,13 +7,13 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Homepage for Cassive Mocks.</p>
-  <p>Sample Text.</p>
-  <p>Sample Text.</p>
-  <p>Sample Text.</p>
-  <p>Sample Text.</p>
-  <p>Sample Text.</p>
-  <p>Sample Text.</p>
+  <p>Homepage for Cassive Mocks</p>
+  <p>Week 3 Preview: Rivalry Week</p>
+  <p>We have @Ryan Hanrahan vs @Jay Lubin in the battle of the meatheads. We all know Hanny can bench more than Jay, but can he break his 0-2 finish against the unlikely 2-0 2Beers1Kupp</p>
+  <p>A feud dating back to Jersey elementary school @Matt Wilen takes on @Mike Mcd. Just gonna take a guess here and call this one the mathlete vs the athlete. But I didn't know them way back when to confirm</p>
+  <p>Last year, @Luke Benjamin got the better of @Brady Marotta in this newly formed heated rivalry. But Brady is ready for round two with his newly revamped RB core</p>
+  <p>Much like that meme of the cheating boyfriend, @Evan Hoffman is looking over his shoulder at his true rival @Tom Minieri. But that's not how the schedule shook out this year and he's stuck facing formal rivalry fling (me). Trust be told. This rivalry mimics more the Pats and Jets than it does the Ravens and Steelers. As one side is a perennial juggernaut while the other has a ton of pre season hype before their 0-2 start</p>
+  <p>Last up is @Tom Minieri and @Greg Bargiel who are too boring to really have any passion between them. Maybe talk some shit so I have something to write about when we all play again in week 12</p>
 `;
 
 /*   STEP 3   */
@@ -32,16 +32,16 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Upper East Side", // (optional)
       "bio": "Commish",
-      "photo": "/managers/jeshiva.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/Managers/jeshiva.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Hanrahan", // Can be anything (usually your rival's name)
-        link: 2, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/hanrahan.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        link: 1, // manager array number within this array, or null to link back to all managers page
+        image: "/Managers/hanrahan.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-      "favoritePlayer": 4214, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "favoritePlayer": 6945, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Your fantasy team's philosophy",
@@ -54,14 +54,14 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Jersey", // (optional)
       "bio": "Lawyer",
-      "photo": "/managers/hanrahan.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/Managers/hanrahan.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Minieri", // Can be anything (usually your rival's name)
-        link: 3, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/minieri.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        link: 2, // manager array number within this array, or null to link back to all managers page
+        image: "/Managers/minieri.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 5917, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "TE", // (optional) Favorite position (QB, WR, RB, TE, etc.)
@@ -76,14 +76,14 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn", // (optional)
       "bio": "Photographer",
-      "photo": "/managers/minieri.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/Managers/minieri.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Jeshiva", // Can be anything (usually your rival's name)
-        link: 1, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/jeshiva.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        link: 0, // manager array number within this array, or null to link back to all managers page
+        image: "/Managers/jeshiva.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 4866, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
